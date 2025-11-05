cd87ceb56a81b2db35e7c9f408c95c4e6967c47e
Sujal Valsur <sujalvalsur11@gmail.com>
2025-11-05 23:51:44 +0530

    update

--- frontend/src/components/ChatBot.jsx
+++ frontend/src/components/ChatBot.jsx
@@ -6,7 +6,7 @@ const ChatBot = () => {
     const { onSent, recentPrompt, result, loading, resultData, setInput, input } = useContext(StoreContext);
 
     return (
-        <div className='h-[90vh] flex items-start flex-col w-[80vw] m-auto mt-[100px] '>
+        <div className='h-[90vh] flex items-start flex-col w-[90vw] lg:w-[80vw] m-auto mt-[100px] '>
             <Link to='/' className='w-full'><div className='w-full flex items-center gap-3 text-[16px] text-gray-500 mb-5'>
                 <i className='fa fa-arrow-left'></i>
                 <p>Back to Home</p>
@@ -14,14 +14,14 @@ const ChatBot = () => {
 
             <div>
                 {!result
-                    ? <div className='w-[60vw] px-5 mt-[100px] '>
+                    ? <div className='w-[90vw] lg:w-[70vw] px-5 lg:mt-[100px] mt-[40px] '>
                         <div className='w-full'>
                             <div>
-                                <p className='text-7xl w-fit bg-gradient-to-r from-blue-500 via-indigo-500 to-orange-700 bg-clip-text text-transparent font-[600]'>Hello, User.</p>
-                                <p className='text-5xl w-fit text-gray-700/90 font-[600] mt-3'>How can I help you today?</p>
+                                <p className='text-5xl lg:text-7xl w-fit bg-gradient-to-r from-blue-500 via-indigo-500 to-orange-700 bg-clip-text text-transparent font-[600]'>Hello, User.</p>
+                                <p className='text-3xl lg:text-5xl w-fit text-gray-700/90 font-[600] mt-3'>How can I help you today?</p>
                             </div>
 
-                            <div className='mt-[200px] w-full flex items-center justify-around gap-5'>
+                            <div className='mt-[100px] w-full flex items-center justify-around lg:flex-nowrap flex-wrap gap-5'>
                                 <div className='w-[250px] bg-blue-400/10 h-[200px] p-5 rounded-[15px] text-[15px] relative'>
                                     <p>Suggest beautiful places to see an on upcoming road trip</p>
                                     <i className='absolute bottom-3 right-3 bg-gray-500/40 h-5 w-5 p-5 flex items-center justify-center rounded-full text-[16px] fa fa-compass'></i>
@@ -42,8 +42,8 @@ const ChatBot = () => {
                         </div>
                     </div>
                     : <div>
-                        <div className='w-[70vw] m-auto'>
-                            <div className='my-[40px] w-full flex items-center justify-end'>
+                        <div className='w-[90vw] lg:w-[70vw] m-auto'>
+                            <div className='my-[10px] lg:my-[40px] w-full flex items-center justify-end'>
                                 <p className=' w-fit py-2 px-3 rounded-md que-animation '>{recentPrompt}</p>
                             </div>
                             {loading
@@ -56,7 +56,7 @@ const ChatBot = () => {
                                         </div>
                                     </div>
                                 </>
-                                : <div className='w-[90%] flex items-start flex-col gap-4'>
+                                : <div className='w-[90%] flex items-start flex-wrap lg:flex-nowrap gap-2 lg:gap-4 mb-[100px] lg:mb-0'>
                                     <i className='bx bxs-sparkles text-[30px] text-blue-400 p-2'></i>
                                     <p className='mt-2 p-3 bg-gray-600/20 rounded-lg'>{resultData}</p>
                                 </div>
@@ -66,7 +66,7 @@ const ChatBot = () => {
                     </div>
                 }
             </div>
-            <div className='w-[50%] m-auto mt-[50px] mb-10 sticky bottom-4 left-[25%] '>
+            <div className='w-[95%] lg:w-[50%] m-auto mt-[50px] mb-2 lg:mb-10 fixed bottom-0 lg:bottom-4 left-[2.5%] lg:left-[25%] '>
                 <div className='w-full flex items-center gap-5 bg-gray-950/95 border-2 border-gray-500/30 rounded-md hover:border-blue-500/80 hover:shadow-[0_0_5px_1px_#73A4FF] shadow-[0_0_5px_1px_#9d9d9d] p-2 '>
                     <input className='w-full h-[2vh] bg-transparent border-none pl-3 outline-none' type="text" onChange={(e) => setInput(e.target.value)} value={input} placeholder='Enter prompt here' />
                     <button onClick={() => { onSent() }} className='bg-blue-400/80 px-3 py-2 text-[20px] rounded-lg hover:bg-blue-500/70 '>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,18 +14,18 @@ const Navbar = () => {
   }
   return (
     <div className=' fixed top-0 left-0 w-[100vw] border-b-2 border-gray-700 backdrop-blur-[5px] z-10'>
-      <div className=' w-[90vw] sm:w-[80vw] md:-[80vw] lg-[80vw] m-auto flex items-center justify-between'>
+      <div className=' w-[90vw] sm:w-[80vw] md:-[80vw] lg-[80vw] m-auto flex items-center justify-between py-3 lg:py-0'>
         <div className='flex items-center gap-2'>
-          <i className='bx  bx-book-open text-[30px] h-[45px] w-[45px] p-5 bg-gradient-to-b from-blue-700 via-blue-500 to-blue-400 flex items-center justify-center rounded-[10px] shadow-lg'></i>
-          <Link to='/'><h1 className='text-[50px] cursor-pointer bg-gradient-to-r from-blue-500 via-cyan-400 to-orange-600 bg-clip-text text-transparent '>BroDocs</h1></Link>
+          <i className='bx  bx-book-open lg:text-[30px] text-[20px] lg:h-[45px] h-[30px] lg:w-[45px] w-[30px] lg:p-5 bg-gradient-to-b from-blue-700 via-blue-500 to-blue-400 flex items-center justify-center lg:rounded-[10px] rounded-md shadow-lg'></i>
+          <Link to='/'><h1 className='lg:text-[50px] text-[25px] cursor-pointer bg-gradient-to-r from-blue-500 via-cyan-400 to-orange-600 bg-clip-text text-transparent '>BroDocs</h1></Link>
         </div>
         <div className='flex items-center gap-3'>
-          <Link to='/ai'><div className='group cursor-pointer py-2 px-3 bg-blue-500/90 border-2 border-gray-700/40 shadow-lg rounded-full flex items-center gap-4'>
-            <i className='bx  bx-sparkles text-3xl' ></i> 
-            <p className='hidden group-hover:block text-[20px] group-hover:transition group-hover:duration-300'>AI Preference</p>
+          <Link to='/ai'><div className='group cursor-pointer lg:py-2 py-1 lg:px-3 px-2 bg-blue-500/90 border-2 border-gray-700/40 shadow-lg rounded-full flex items-center lg:gap-4 gap-1'>
+            <i className='bx  bx-sparkles text-xl lg:text-3xl' ></i> 
+            <p className='hidden group-hover:block lg:text-[20px] text-[20px] group-hover:transition group-hover:duration-300'>AI</p>
           </div></Link>
-          <div onClick={() => logout()} className=' cursor-pointer py-2 px-4 bg-blue-500/90 hover:bg-blue-500/70 border-2 border-gray-700/40 shadow-lg rounded-lg flex items-center gap-4'>
-            <i className="fa fa-arrow-right-from-bracket text-2xl "></i>
+          <div onClick={() => logout()} className=' cursor-pointer lg:py-2 py-1 lg:px-4 px-2 bg-blue-500/90 hover:bg-blue-500/70 border-2 border-gray-700/40 shadow-lg rounded-lg flex items-center lg:gap-4 gap-2'>
+            <i className="fa fa-arrow-right-from-bracket lg:text-2xl text-xl "></i>
             <p>Sign Out</p>
           </div>
         </div>
